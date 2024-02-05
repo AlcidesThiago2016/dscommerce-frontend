7520818942e6ce910a60b848512c84f55006157d
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-02-05 16:32:46 -0300

    Dynamic Routes

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,36 +2,22 @@ import './styles.css'
 import BottonInverse from "../../../components/BottonInverse";
 import BottonPrimary from "../../../components/BottonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { ProductDTO } from '../../../models/product';
-
-const product: ProductDTO = {
-    id: 2,
-    name: "Smart TV",
-    description: "Esta TV e muito bonita",
-    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
-    price: 2500.99,
-    categories: [
-        {
-            id: 2,
-            name : "Eletrônicos"
-        },
-        {
-            id: 3,
-            name : "Computadores"
-        },
-        {
-            id: 4,
-            name: "Importados"
-        }
-    ]
-
-}
+import * as productService from '../../../services/product-service';
+import { useParams } from 'react-router-dom';
 
 export default function ProductDetails(){
+
+    const params = useParams();
+
+    const product = productService.findById(Number(params.productId));
+
     return(
         <main>
             <section id="product-details-section" className="dsc-container">
-                <ProductDetailsCard product= {product} />
+                {
+                    product &&
+                     <ProductDetailsCard product= {product} />
+                }
                 <div className="dsc-btn-page-container">
                     <BottonPrimary text="Comprar"/>
                     <BottonInverse text="Inicio"/>
