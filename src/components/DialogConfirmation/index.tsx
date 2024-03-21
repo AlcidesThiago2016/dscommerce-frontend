374f4ca29fd19002e07c018958234b1c401215f9
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-03-21 11:04:31 -0300

    Delete Finish

--- src/components/DialogConfirmation/index.tsx
+++ src/components/DialogConfirmation/index.tsx
@@ -3,22 +3,23 @@ import BottonPrimary from "../BottonPrimary";
 import BottonInverse from "../BottonInverse";
 
 type Props = {
+    id: number;
     message: string;
     onDialogAnswer: Function;
 }
 
 
-export default function DialogConfirmation({ message, onDialogAnswer}: Props){
+export default function DialogConfirmation({ id, message, onDialogAnswer}: Props){
 
     return(
-        <div className="dsc-dialog-background" onClick={() => onDialogAnswer(false)}>
+        <div className="dsc-dialog-background" onClick={() => onDialogAnswer(false, id)}>
             <div className="dsc-dialog-box" onClick={(event) => event.stopPropagation()}>
                 <h2>{message}</h2>
                 <div className="dsc-dialog-btn-container" >
-                    <div onClick={() => onDialogAnswer(false)}>
+                    <div onClick={() => onDialogAnswer(false, id)}>
                         <BottonPrimary  text="Não"/>   
                     </div>
-                    <div onClick={() => onDialogAnswer(true)}>
+                    <div onClick={() => onDialogAnswer(true, id)}>
                         <BottonInverse  text="Sim"/>
                     </div>
                 </div> 
