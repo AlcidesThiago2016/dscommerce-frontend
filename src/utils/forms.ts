2892c18ddece66ae42810b228405e52991df2dc4
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2024-03-22 09:23:20 -0300

    Forms toValues

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -2,3 +2,13 @@
 export function update(inputs: any, name: string, newValue: any){
     return { ...inputs, [name]: { ...inputs[name], value: newValue }};
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function toValue(inputs: any){
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const data: any = {};
+    for (const name in inputs){
+        data[name] = inputs[name].value;
+    }
+    return data;
+}
